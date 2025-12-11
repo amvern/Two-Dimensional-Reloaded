e38b378390c2afa8ff7b58b8325f084d2ca9a613
amvern <drkmannn@gmail.com>
2025-12-10 16:30:31 -0800

    removed satin, added sodium/iris, removed non sodium compat mixins

--- src/client/java/github/mishkis/twodimensional/client/mixin/LevelRendererMixin.java
+++ src/client/java/github/mishkis/twodimensional/client/mixin/LevelRendererMixin.java
@@ -10,30 +10,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
-
-    @ModifyExpressionValue(
-            method = "renderLevel",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/SectionRenderDispatcher$CompiledSection;getRenderableBlockEntities()Ljava/util/List;")
-    )
-    private List<BlockEntity> cullBlockEntities(List<BlockEntity> original) {
-        Plane plane = TwoDimensionalClient.plane;
-        if (plane != null) {
-            original = original.stream().filter(blockEntity ->
-                            !Plane.shouldCull(blockEntity.getBlockPos(), plane))
-                    .toList();
-        }
-
-        return original;
-    }
 
     @Inject(method = "renderHitOutline", at = @At(value = "HEAD"), cancellable = true)
     private void disableCulledBlockOutline(PoseStack matrices, VertexConsumer vertexConsumer, Entity entity, double cameraX, double cameraY, double cameraZ, BlockPos pos, BlockState state, CallbackInfo ci) {
@@ -42,8 +25,4 @@ public class LevelRendererMixin {
         }
     }
 
-//    @ModifyVariable(method = "collectRenderableChunks", at = @At("HEAD"), argsOnly = true)
-//    private boolean disableChunkCulling(boolean value) {
-//        return value && TwoDimensionalClient.plane == null;
-//    }
 }
